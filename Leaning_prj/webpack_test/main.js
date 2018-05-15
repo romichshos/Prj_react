b8dd017b9accea696b3521f1643d889433cb3eb0
Roman K <romichsh@outlook.com>
2018-05-16 01:47:40 +0300

    new

--- Leaning_prj/webpack_test/main.js
+++
@@ -1,4 +0,0 @@
-var sayHello = require('./say-hello');
-var eObj = require('./empty-object');
-sayHello('R', document.querySelector('h2'));
-eObj(document.querySelector('h2'));
--- Leaning_prj/webpack_test/say-hello.js
+++
@@ -1,4 +0,0 @@
-
-module.exports = function (name, element) {
-    element.textContent = 'Hello ' + name + '!';
-};
